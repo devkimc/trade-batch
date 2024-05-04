fdf76ccd06726e43a4369c283456ddc6a587e976
WooSeok Kim <kws60000@naver.com>
2024-05-04 13:19:50 +0900

    feat: 프로젝트 설정

---
+++ src/main/java/com/kr/economy/tradebatch/TradeBatchApplication.java
@@ -0,0 +1,13 @@
+package com.kr.economy.tradebatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradeBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradeBatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kr/economy/tradebatch/TradeBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kr.economy.tradebatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradeBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
