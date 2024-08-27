5deaa76d9699a6204de0d5acf51573184448f038
WooSeok Kim <kws60000@naver.com>
2024-08-27 21:29:48 +0900

    feat: 의존성 주입 실패 에러 개선
    - 필드 주입 방식으로 변경

--- src/main/java/com/kr/economy/tradebatch/config/WebsocketClientEndpoint.java
+++ src/main/java/com/kr/economy/tradebatch/config/WebsocketClientEndpoint.java
@@ -10,7 +10,9 @@ import com.kr.economy.tradebatch.trade.application.queryservices.TradingHistoryQ
 import com.kr.economy.tradebatch.trade.domain.model.aggregates.TradingHistory;
 import jakarta.websocket.*;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.socket.WebSocketMessage;
@@ -32,13 +34,18 @@ public class WebsocketClientEndpoint {
     @Value("${endpoint.kis.trade.socket.host}")
     private String socketUrl;
 
-    Session userSession = null;
-    private KisQuoteService kisQuoteService;
+    @Autowired
     private SharePriceHistoryCommandService sharePriceHistoryCommandService;
+    @Autowired
     private BidAskBalanceCommandService bidAskBalanceCommandService;
+    @Autowired
     private TradingHistoryQueryService tradingHistoryQueryService;
+    @Autowired
     private TradingHistoryCommandService tradingHistoryCommandService;
+    @Autowired
     private KoreaStockOrderQueryService koreaStockOrderQueryService;
+
+    Session userSession = null;
 
     public WebsocketClientEndpoint() {
         try {
@@ -81,60 +88,75 @@ public class WebsocketClientEndpoint {
      */
     @OnMessage
     public void onMessage(String message) {
-        String responseByMessage = getResponseBySocketMessage(message);
+        if (message == null || message.length() < 2) {
+            log.warn("[Socket response] message : " + message);
+            return;
+        }
 
-        if (responseByMessage.length() < 2) return;
-        String[] resultBody = responseByMessage.split("\\|");
+        String[] resultBody = message.split("\\|");
+        if (resultBody.length < 4) {
+            log.info("[Socket response] message : " + message);
+            return;
+        }
 
-        if (resultBody.length < 4) return;
         String[] result = resultBody[3].split("\\^");
+        if (result.length < 38) {
+            log.info("[Socket response] message : " + message);
+            return;
+        }
 
-        System.out.println("message = " + message);
+        try {
+            String tradingTime = result[1];
+            int sharePrice = Integer.parseInt(result[2]);
+            float bidAskBalanceRatio = Float.parseFloat(result[37]) / Float.parseFloat(result[36]);
 
-        String tradingTime = result[1];
-        int sharePrice = Integer.parseInt(result[2]);
-        float bidAskBalanceRatio = Float.parseFloat(result[37]) / Float.parseFloat(result[36]);
+            // 실시간 현재가 저장
+            sharePriceHistoryCommandService.createSharePriceHistory(TICKER_SAMSUNG, sharePrice, tradingTime);
 
-        // 실시간 현재가 저장
-        sharePriceHistoryCommandService.createSharePriceHistory(TICKER_SAMSUNG, sharePrice, tradingTime);
+            // 실시간 매수매도 잔량비 저장
+            bidAskBalanceCommandService.createBidAskBalanceRatioHistory(TICKER_SAMSUNG, bidAskBalanceRatio);
 
-        // 실시간 매수매도 잔량비 저장
-        bidAskBalanceCommandService.createBidAskBalanceRatioHistory(TICKER_SAMSUNG, bidAskBalanceRatio);
+            // 당일 마지막 체결 내역 조회
+            Optional<TradingHistory> lastTradingHistory = tradingHistoryQueryService.getLastHistoryOfToday(TICKER_SAMSUNG);
 
-        // 당일 마지막 체결 내역 조회
-        Optional<TradingHistory> lastTradingHistory = tradingHistoryQueryService.getLastHistoryOfToday(TICKER_SAMSUNG);
-
-        // 마지막 체결 내역이 매수일 경우에만 매도
-        if (lastTradingHistory.isPresent() && lastTradingHistory.get().isBuyTrade()) {
-            if (lastTradingHistory.get().isSellSignal(sharePrice)) {
-                CreateTradingHistoryCommand createTradingHistoryCommand = CreateTradingHistoryCommand.builder()
-                        .ticker(TICKER_SAMSUNG)
-                        .orderDvsnCode("01")
-                        .tradingPrice(sharePrice - 100)
-                        .tradingQty(1)
-                        .tradingResultType("0")
-                        .kisOrderDvsnCode("00")
-                        .kisId("") // TODO 제거하기
-                        .tradingTime("") // TODO 제거하기
-                        .build();
-                tradingHistoryCommandService.createTradingHistory(createTradingHistoryCommand);
-                System.out.println("********************************************* " + tradingTime + " : " + (sharePrice - 100) + " 매도 체결 ********************************************* ");
+            // 마지막 체결 내역이 매수일 경우에만 매도
+            if (lastTradingHistory.isPresent() && lastTradingHistory.get().isBuyTrade()) {
+                if (lastTradingHistory.get().isSellSignal(sharePrice)) {
+                    CreateTradingHistoryCommand createTradingHistoryCommand = CreateTradingHistoryCommand.builder()
+                            .ticker(TICKER_SAMSUNG)
+                            .orderDvsnCode("01")
+                            .tradingPrice(sharePrice - 100)
+                            .tradingQty(1)
+                            .tradingResultType("0")
+                            .kisOrderDvsnCode("00")
+                            .kisId("") // TODO 제거하기
+                            .tradingTime("") // TODO 제거하기
+                            .build();
+                    tradingHistoryCommandService.createTradingHistory(createTradingHistoryCommand);
+                    System.out.println("************ " + tradingTime + " : " + (sharePrice - 100) + " 매도 체결 ************");
+                }
+            } else {
+                if (koreaStockOrderQueryService.getBuySignal(TICKER_SAMSUNG)) {
+                    CreateTradingHistoryCommand createTradingHistoryCommand = CreateTradingHistoryCommand.builder()
+                            .ticker(TICKER_SAMSUNG)
+                            .orderDvsnCode("02")
+                            .tradingPrice(sharePrice + 100)
+                            .tradingQty(1)
+                            .tradingResultType("0")
+                            .kisOrderDvsnCode("00")
+                            .kisId("") // TODO 제거하기
+                            .tradingTime("") // TODO 제거하기
+                            .build();
+                    tradingHistoryCommandService.createTradingHistory(createTradingHistoryCommand);
+                    System.out.println("************ " + tradingTime + " : " + (sharePrice + 100) + " 매수 체결 ************");
+                }
             }
-        } else {
-            if (koreaStockOrderQueryService.getBuySignal(TICKER_SAMSUNG)) {
-                CreateTradingHistoryCommand createTradingHistoryCommand = CreateTradingHistoryCommand.builder()
-                        .ticker(TICKER_SAMSUNG)
-                        .orderDvsnCode("02")
-                        .tradingPrice(sharePrice + 100)
-                        .tradingQty(1)
-                        .tradingResultType("0")
-                        .kisOrderDvsnCode("00")
-                        .kisId("") // TODO 제거하기
-                        .tradingTime("") // TODO 제거하기
-                        .build();
-                tradingHistoryCommandService.createTradingHistory(createTradingHistoryCommand);
-                System.out.println("********************************************* " + tradingTime + " : " + (sharePrice + 100) + " 매수 체결 ********************************************* ");
-            }
+        } catch (DataAccessException dae) {
+            log.error("[Socket response DB 에러] exception : " + dae);
+            log.error("[Socket response DB 에러] message : " + message);
+        } catch (RuntimeException re) {
+            log.error("[Socket response 런타임 에러] exception : " + re);
+            log.error("[Socket response 런타임 에러] message : " + message);
         }
     }
 
@@ -150,20 +172,5 @@ public class WebsocketClientEndpoint {
      */
     public void sendMessage(String message) {
         this.userSession.getAsyncRemote().sendText(message);
-    }
-
-    /**
-     * 소켓 메시지로부터 문자열 응답값을 추출
-     * @param message
-     * @return
-     */
-    private String getResponseBySocketMessage(String message) {
-        String response = "";
-
-        if (message == null || !StringUtils.hasLength(message)) {
-            log.warn("[Socket Response] 메시지 미존재 message : " + message);
-        }
-
-        return response;
     }
 }
